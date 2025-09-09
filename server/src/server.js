81724b4e3afb74aa9744f3475d7f4b851c90869d
Vishesh Kumar <mrvisheshk@gmail.com>
2025-09-09 23:39:58 +0530

    env added

--- server/src/server.js
+++ server/src/server.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json()); 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
